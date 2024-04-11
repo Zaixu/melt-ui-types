0a7b85b26bcd07ae83cd253676d26bf6fbd91d58
Zaixu <quottrup@outlook.com>
2024-04-11 23:45:25 +0200

    First

---
+++ src/lib/Button.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { type AnyMeltElement, emptyMeltElement } from '@melt-ui/svelte'
+
+    export let element: AnyMeltElement = emptyMeltElement
+</script>
+
+<button {...$element} use:element class="grid h-9 w-9 place-items-center items-center justify-center rounded-md
+bg-white text-base leading-4 text-magnum-800 shadow-lg hover:bg-magnum-100
+  data-[disabled]:cursor-not-allowed data-[state=on]:bg-magnum-200
+  data-[state=on]:text-magnum-900">
+    <slot />
+</button>
---
+++ src/lib/Toggle.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { createToggle, melt } from '@melt-ui/svelte';
+	import Button from './Button.svelte';
+  
+    const {
+      elements: { root },
+    } = createToggle();
+  </script>
+  
+  <Button element={root}>
+    ABC
+  </Button>
